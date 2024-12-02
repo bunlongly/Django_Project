713563f606f322ae120ef618a62a76e3d871b2a1
SQA <bunlongly03@gmail.com>
2024-12-02 15:00:24 +0700

    fix api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,17 +1,24 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+// Create an axios instance
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem(ACCESS_TOKEN);
-  if (token) {
-    config.headers.Authorization = "Bearer ${token}";
-  }
+// Request interceptor to add the authorization token if available
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      // Correct string interpolation using backticks (`) for template literals
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config; // Don't forget to return the config object here!
+  },
   (error) => {
-    return Promise.reject(error);
-  };
-});
+    return Promise.reject(error); // Handle any request errors
+  }
+);
+
 export default api;
